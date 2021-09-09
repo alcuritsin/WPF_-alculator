d262008077198546cc8b61843da7ed121d7a87cc
Курицын Алексей <al.curitsin@gmail.com>
2021-09-09 22:55:46 +0500

    View Form -done

---
+++ Сalculator/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Сalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
